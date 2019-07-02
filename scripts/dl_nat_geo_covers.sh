799d7ef734ccaea4c1dfda4eac1d7a91e53a6231
Jared Wilber <j.wilber92@gmail.com>
2019-07-02 14:03:41 -0700

    updated download script

--- scripts/dl_nat_geo_covers.sh
+++ scripts/dl_nat_geo_covers.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# download National Geographic magazine covers and save them to data/
+# download National Geographic magazine covers and save them to ../data/images/
 
 set -o errexit
 set -o pipefail
 
+string_split="?type"
+
 IFS=','
-while read c1 cover_image_url c3 c4; do
-    wget "$cover_image_url" -P data/images
+while read c1 cover_image_url month year; do
+	# extract url (just jpg part)
+	image_url=${cover_image_url%$string_split*}
+	# specify save path
+	save_path="../data/images/cover_${month}_${year}.jpg"
+	# download image
+    wget "$image_url" -O $save_path  
 done < ../data/national_geographic_covers.csv
